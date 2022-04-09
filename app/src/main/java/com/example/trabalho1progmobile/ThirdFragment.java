9ce115ec30ad70d49cea2d332f0b0d5e1742905b
Samuel Willian De Souza Moraes <samuel@jeraapps.com>
2022-04-09 17:26:38 -0400

    [NEW] FRAGMENTS

---
+++ app/src/main/java/com/example/trabalho1progmobile/ThirdFragment.java
@@ -0,0 +1,41 @@
+package com.example.trabalho1progmobile;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.trabalho1progmobile.databinding.FragmentThirdBinding;
+
+public class ThirdFragment extends Fragment {
+    private FragmentThirdBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_ThirdFragment_to_FirstFragment);
+            }
+        });
+    }
+}
